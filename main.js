1838f7114896290b10f98731b9c0bff50d6f9e12
suniLPATIDARr <sunilpatidar1209@gmail.com>
2022-04-17 00:27:47 +0530

    ts initials

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var message = 'this is msg';
+console.log(message);
+//compile this down to plain js by using ts compiler
+//by adding import or export statement typescript treat this file as a module instead of script
+//automatically recompile the JS file if there is any change by using - tsc main --watch
---
+++ main.ts
@@ -0,0 +1,6 @@
+export { }
+let message = 'this is msg';
+console.log(message);
+
+//compile this down to plain js by using ts compiler
+//by adding import or export statement typescript treat this file as a module instead of script
